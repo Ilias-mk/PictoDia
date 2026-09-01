828cc66646d7ad0084bdefa10ff8ff29a5dd046a
iliasmk50-sketch <iliasmk50@gmail.com>
2026-09-01 17:33:21 +0200

    Initial Commit

---
+++ PictoDia/PictoDiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PictoDiaApp.swift
+//  PictoDia
+//
+//  Created by Ilias Mohamed on 2026-09-01.
+//
+
+import SwiftUI
+
+@main
+struct PictoDiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
